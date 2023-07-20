71dae18f9c0cbff1c596eaa3a4aa31073e2d1d7a
Adriano <adriano_alves_2008@hotmail.com>
2023-07-20 11:26:30 -0300

    Feat: Create animation welcome

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,18 +1,24 @@
 import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 
+import * as Animatable from 'react-native-animatable';
+
 const Welcome = () => {
   return (
     <View style={styles.container}>
       <View style={styles.containerLogo}>
-        <Image
+        <Animatable.Image
+          animation="flipInY"
           source={require('../../assets/logo.png')}
           style={{width: '100%'}}
           resizeMode="contain"
         />
       </View>
 
-      <View style={styles.containerForm}>
+      <Animatable.View
+        delay={600}
+        animation="fadeInUp"
+        style={styles.containerForm}>
         <Text style={styles.title}>
           Monitore e organize os seus gastos de qualquer lugar!
         </Text>
@@ -21,7 +27,7 @@ const Welcome = () => {
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Acessar</Text>
         </TouchableOpacity>
-      </View>
+      </Animatable.View>
     </View>
   );
 };
